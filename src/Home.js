570896502b225b47bc7ab77572751a1eebf135f6
CaiqueRibe <33009980+CaiqueRibe@users.noreply.github.com>
2023-08-04 22:06:19 -0300

    tillJsonServer

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,97 @@
+import { useEffect, useState } from "react"
+import BlogList from "./BlogList"
+
 const Home = () => {
-   const handleClick = (e) => {
-      // naming convention
-      console.log("Hello Ninjas", e)
+   const [blogs, setBlogs] = useState(null)
+   // {
+   //    title: "My new teste1",
+   //    body: "lorem ipsum...",
+   //    author: "mario",
+   //    id: 1,
+   // },
+   // {
+   //    title: "My new teste2",
+   //    body: "lorem ipsum...",
+   //    author: "yoshi",
+   //    id: 2,
+   // },
+   // {
+   //    title: "My new teste3",
+   //    body: "lorem ipsum...",
+   //    author: "mario",
+   //    id: 3,
+   // },
+
+   const [name, setName] = useState("mario")
+
+   const handleDelete = (id) => {
+      // const newBlogs = blogs.filter((blog) => blog.id !== id)
+      // setBlogs(newBlogs)
+
+      setBlogs((currentBlogs) => {
+         const teste2 = [...currentBlogs]
+         const teste = teste2.findIndex((e) => e.id === id)
+
+         if (teste !== -1) {
+            teste2.splice(teste, 1)
+         }
+
+         return teste2
+         // return teste2.splice(teste, 1)
+      })
+      // setBlogs((currentBlogs) => {
+      //    const updatedBlogs = [...currentBlogs]
+      //    const indexToDelete = updatedBlogs.findIndex((blog) => blog.id === id)
+
+      //    if (indexToDelete !== -1) {
+      //       updatedBlogs.splice(indexToDelete, 1)
+      //    }
+
+      //    return updatedBlogs
+      // })
+
+      // setTodos((curerentTodos) => {
+      //    const teste2 = [...curerentTodos]
+      //    const teste = teste2.findIndex((e) => e.id === id)
+      //    return teste2.splice(teste, 1, teste2)
+      //    // return curerentTodos.filter((todo) => todo.id !== id) // this will return all the currentodos execept the one i dicided to remove(deleted)
+      // })
    }
 
-   const handleClickButton2 = (name, e) => {
-      console.log("Hello " + name, e.target) //e.target = <button>CLick me too</button>
-   }
+   useEffect(() => {
+      console.log("Running Use Effect")
+      // console.log(name)
+      // console.log(blogs)
+      fetch("http://localhost:8000/blogs") // remenber that this isent the data its an object
+         .then((res) => {
+            return res.json()
+         })
+         .then((data) => {
+            // console.log(data)
+            setBlogs(data)
+         })
+   }, [])
+   // }, [name])
 
    return (
       <div className="home">
-         <h2>Homepage</h2>
-         <button onClick={handleClick}>Click Me</button>{" "}
-         {/* function reference vs invoke function  */}
-         <button onClick={(e) => handleClickButton2("Caique", e)}>
-            Click me Too
+         {blogs && (
+            <BlogList
+               blogs={blogs}
+               title="All Blogs Listed"
+               handleDelete={handleDelete}
+            />
+         )}
+         <button onClick={() => setName(name === "Mario" ? "Luigi" : "Mario")}>
+            Change Name
          </button>
-         {/* so instead of passing something as a paremeter we would use a wrap of a annoynymous function, not invoking it... */}
+         <p>{name}</p>
+         {/* <BlogList
+            blogs={blogs.filter((blog) => blog.author === "mario")}
+            title="Mario's Blogs"
+         /> */}
+         {/* <BlogList blogs={blogs} title="All Blogs Listed" /> */}
+         {/* <BlogList blogs={blogs} /> */}
       </div>
    )
 }
